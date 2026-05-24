f46494f5e387f87fc48718fb9ae43136c20e1901
darya-loboca <dasha.lobova.it@gmail.com>
2026-05-24 13:40:40 +0300

    add method toString() and function print()

--- MainProject/Flower.h
+++ MainProject/Flower.h
@@ -3,10 +3,45 @@
 using namespace std;
 
 class Flower {
+public:
 	string name;
 	string color;
 	string region;
 	double price;
 	int freshness_days;
 	bool is_blooming;
+
+	//default constructor / constructor without arguments
+	Flower() {
+		name = "";
+		color = "";
+		region = "";
+		price = 0.0;
+		freshness_days = 0;
+		is_blooming = 0;
+	}
+
+	//canonical constructor / constructor with arguments
+	Flower(string nm, string clr, string rgn, 
+		double prc, int fresh_days, bool is_bloom) {
+		name = nm;
+		color = clr;
+		region = rgn;
+		price = prc;
+		freshness_days = fresh_days;
+		is_blooming = is_bloom;
+	}
+
+	string toString() {
+		string s = "Flower: ";
+		s += "name = " + name;
+		s += ", color = " + color;
+		s += ", region = " + region;
+		s += ", price = " + to_string(price);
+		s += ", days in vase = " + to_string(freshness_days);
+		s += ", is blooming? = ";
+		s += (is_blooming ? "yes" : "no");
+
+		return s;
+	}
 };
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,6 +1,19 @@
 #include "Flower.h"
 
+void print(Flower flower) {
+	cout << flower.toString() << endl;
+}
+
 int main() {
+
+	Flower tulip("Tulip", "pink", "Netherlands", 2.5, 3, true);
+	Flower rose("Rose", "red", "Equador", 2.0, 1, false);
+	Flower orchid("Orchid", "white", "Thailand", 5.0, 5, true);
+
+	print(tulip);
+	print(rose);
+	print(orchid);
+
 
 	return 0;
 }
